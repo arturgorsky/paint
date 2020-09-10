f66b932f5e9be55ccc05c86e6e8d0d180c1d5e80
arturgorsky <gorski.artur1@gmail.com>
2020-09-10 23:07:19 +0200

    Added color picker tool

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const canvas = document.getElementById("canvas");
 const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const sizeDisplay = document.getElementById("size");
+const colorPicker = document.getElementById("color");
 
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
-
-// Some hardcoded values
-let size = 10; //paint size
-let x = 50;
-let y = 50;
+let size = 10; //brush size
 let isMouseDown = false;
+let color = 'black';
+let x = undefined;
+
+ctx.lineWidth = size;
+//values for drawing lines
 
 
 // drawing(swipe) event listeners
@@ -37,19 +39,34 @@ increaseBtn.addEventListener("click", () => {
     size += 2;
     updateSizeDisplay();
   }
-})
+});
 
 decreaseBtn.addEventListener("click", () => {
   if(size > 2) {
     size -= 2;
     updateSizeDisplay();
   }
+});
+
+colorPicker.addEventListener("change", (e) => {
+  let color = e.target.value;
+  ctx.fillStyle = color;
 })
+
 
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, 2 * Math.PI);
   ctx.fill();
+}
+
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.lineWidth = size;
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
 }
 
 function updateSizeDisplay() {
